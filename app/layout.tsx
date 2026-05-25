8d8639f38318115d2a20e233e9cb739fdf66677c
Brinley Macnamara <brinleymacnamara@Brinleys-MacBook-Pro.local>
2026-05-24 22:50:30 -0700

    homepage and initial global styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Fraunces } from "next/font/google";
 import "./globals.css";
+
+const fraunces = Fraunces({
+  subsets: ["latin"],
+  variable: "--font-fraunces",
+});
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +31,9 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${fraunces.variable} ${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="max-w-xl mt-40 mx-auto flex flex-col">{children}</body>
     </html>
   );
 }
--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,6 +1,10 @@
 import type { MDXComponents } from 'mdx/types';
  
-const components: MDXComponents = {};
+const components: MDXComponents = {
+  h1: ({ children }) => (
+    <h1 className="font-serif py-2 text-xl">{children}</h1>
+  ),
+};
  
 export function useMDXComponents(): MDXComponents {
   return components
